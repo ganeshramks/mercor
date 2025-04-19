e03bbf4f44ee6002c3b1571741f55a9816777952
ganeshramks <ganeshram997@gmail.com>
2025-04-19 12:45:31 +0530

    connect to db test

--- main-lambda/main.go
+++ main-lambda/main.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"fmt"
-	"common-libs/utility"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql" // import your used driver
 )
 
-func main(){
-	fmt.Println(Handler(3));
+// Model Struct
+type User struct {
+	Id   int    `orm:"auto"`
+	Name string `orm:"size(100)"`
 }
 
-func Handler(x int) (y int) {
-	utility.CPrint()
-	return x;
+func init() {
+	fmt.Println("init")
+	_, err := orm.GetDB("mercor")
+	if err != nil {
+		err = orm.RegisterDataBase("mercor", "mysql", "mercor:mercor123@/mercor?charset=utf8")
+	}
+	fmt.Println("error:", err)
+}
+
+func main() {
+
+	o := orm.NewOrm()
+	user := User{Name: "slene"}
+	// insert
+	id, err := o.Insert(&user)
+	// update
+	user.Name = "astaxie"
+	num, err := o.Update(&user)
+	// read one
+	u := User{Id: user.Id}
+	err = o.Read(&u)
+	// delete
+	num, err = o.Delete(&u)
+
+	fmt.Println("num: ", num, "err:", err, "id: ", id)
 }
